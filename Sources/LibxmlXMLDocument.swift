22ead12eac2e759059084d0b6da9583cebb722e1
broadwaylamb <seergy_pro@me.com>
2016-09-12 01:54:31 +0300

    [Linux] Fix self capture during initialization

--- Sources/LibxmlXMLDocument.swift
+++ Sources/LibxmlXMLDocument.swift
@@ -17,33 +17,6 @@ internal final class LibxmlXMLDocument: XMLDocument {
     private var url: String?
     private var encoding: String.Encoding
     
-    #if !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
-    private let encodingIANANames: [String.Encoding : String] = [
-    .ascii              : "us-ascii",
-    .nextstep           : "x-nextstep",
-    .japaneseEUC        : "euc-jp",
-    .utf8               : "utf-8",
-    .isoLatin1          : "iso-8859-1",
-    .symbol             : "x-mac-symbol",
-    .shiftJIS           : "cp932",
-    .isoLatin2          : "iso-8859-2",
-    .unicode            : "utf-16",
-    .windowsCP1251      : "windows-1251",
-    .windowsCP1252      : "windows-1252",
-    .windowsCP1253      : "windows-1253",
-    .windowsCP1254      : "windows-1254",
-    .windowsCP1250      : "windows-1250",
-    .iso2022JP          : "iso-2022-jp",
-    .macOSRoman         : "macintosh",
-    .utf16              : "utf-16",
-    .utf16BigEndian     : "utf-16be",
-    .utf16LittleEndian  : "utf-16le",
-    .utf32              : "utf-32",
-    .utf32BigEndian     : "utf-32be",
-    .utf32LittleEndian  : "utf-32le"
-    ]
-    #endif
-    
     init?(xml: String, url: String? = nil, encoding: String.Encoding, options: XMLParserOptions) {
         _xml  = xml
         self.url  = url
@@ -151,4 +124,31 @@ internal final class LibxmlXMLDocument: XMLDocument {
         return rootNode?.search(byCSSSelector: selector, namespaces: namespaces) ?? .none
     }
 }
+
+#if !(os(macOS) || os(iOS) || os(watchOS) || os(tvOS))
+private let encodingIANANames: [String.Encoding : String] = [
+    .ascii              : "us-ascii",
+    .nextstep           : "x-nextstep",
+    .japaneseEUC        : "euc-jp",
+    .utf8               : "utf-8",
+    .isoLatin1          : "iso-8859-1",
+    .symbol             : "x-mac-symbol",
+    .shiftJIS           : "cp932",
+    .isoLatin2          : "iso-8859-2",
+    .unicode            : "utf-16",
+    .windowsCP1251      : "windows-1251",
+    .windowsCP1252      : "windows-1252",
+    .windowsCP1253      : "windows-1253",
+    .windowsCP1254      : "windows-1254",
+    .windowsCP1250      : "windows-1250",
+    .iso2022JP          : "iso-2022-jp",
+    .macOSRoman         : "macintosh",
+    .utf16              : "utf-16",
+    .utf16BigEndian     : "utf-16be",
+    .utf16LittleEndian  : "utf-16le",
+    .utf32              : "utf-32",
+    .utf32BigEndian     : "utf-32be",
+    .utf32LittleEndian  : "utf-32le"
+]
+#endif
 
